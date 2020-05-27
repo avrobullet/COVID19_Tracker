25acfcec8807e82d1295792e1fbd1da3a26582af
Alexander Douglas <alexanderdouglas@MacBook-Air.local>
2020-05-27 13:48:45 -0700

    Version 9.3: added latest update date/time, 3D card scaling, card shadows, stat fonts and colours, and created a canadian land dictionary

--- js/GetStats.js
+++ js/GetStats.js
@@ -1,17 +1,37 @@
+//Months...
+months = ['January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
 // Canadian Provinces and Territories (in no particular order)
-canadian_provinces_territories = ['Alberta',
-    'British Columbia',
-    'Nova Scotia',
-    'Quebec',
-    'Ontario',
-    'New Brunswick',
-    'Manitoba',
-    'Prince Edward Island',
-    'Saskatchewan',
-    'Newfoundland and Labrador',
-    'Yukon',
-    'Nunavut',
-    'Northwest Territories']
+canadian_stats = {
+    land: ['Alberta',
+        'British Columbia',
+        'Nova Scotia',
+        'Quebec',
+        'Ontario',
+        'New Brunswick',
+        'Manitoba',
+        'Prince Edward Island',
+        'Saskatchewan',
+        'Newfoundland and Labrador',
+        'Yukon',
+        'Nunavut',
+        'Northwest Territories'],
+    active:['blue','25px'],
+    confirmed: ['orange','25px'],
+    deaths:['red','25px'],
+    recovered: ['green','25px']
+}
 // Create functions
 function getProvinceStats() {
     //Get Canadian COVID19 stats
@@ -30,22 +50,29 @@ function getProvinceStats() {
     request.send()
 }
 function displayProvinceStats(stats){
-    //if (new_province_selected ==> display stats)
-    //else (previous_province_selected ==> page remains unchanged)
     //Collect COVID19 confirmed, deaths, and recoveries (COVID19 stats)
-    console.log(stats)
-    for (let key in canadian_provinces_territories) {
-        selected_province_territory = canadian_provinces_territories[key]
+    for (let key in canadian_stats.land) {
+        selected_province_territory = canadian_stats.land[key]
         for (let key in stats) {
-            console.log(stats[key].Recovered)
+            // Process province's/territories' corresponding data
             if (selected_province_territory.toLowerCase().trim() == stats[key].Province.toLowerCase().trim()) {
                 valid_land = true
                 current_province_key = key
-                calculateData(key,stats)
+                processData(key,stats)
             }
         }
         // Only create COVID19 stats if currently selected province/territory is found
         if (valid_land == true) {
+            //Post latest COVID19 time stamp
+            let date = current_timestamp.slice(0, 10)
+            let time = current_timestamp.slice(11, 19)
+            date = new Date(date)
+            let month = months[date.getMonth()]+' '
+            let day = date.getDay()+''
+            let year = date.getFullYear()+''
+            new_date = month.concat(day,' ,',year)
+            document.getElementById("timestamp_update").innerHTML = 'As of '+new_date;
+
             //Post COVID19 stats onto 'cards'
             const card = document.createElement('div')
             card.setAttribute('class', 'card')
@@ -53,9 +80,9 @@ function displayProvinceStats(stats){
             //Store COVID19 stats to display as a table (Convert to dictionnary where each label includes a type of colour)
             const table = document.createElement("table")
             selected_display_stats = {
-                "Currently Known Cases": current_confirmed,
-                "Currently Known Deaths": current_deaths,
-                "Currently Known Active": current_active,
+                "Confirmed Cases": current_confirmed,
+                "Confirmed Deaths": current_deaths,
+                "Confirmed Active": current_active,
                 "Total Recovered": total_recovered
             }
 
@@ -73,10 +100,17 @@ function displayProvinceStats(stats){
         valid_land = false
     }
 }
-function calculateData(key,stats) {
+function processData(key,stats) {
+    // Graphing data
+    accumulated_active += stats[key].Active
+    accumulated_confirmed += stats[key].Confirmed
+    accumulated_deaths += stats[key].Deaths
+    accumulated_recovered += stats[key].Date
+    // Main data
     current_active = stats[key].Active
     current_confirmed = stats[key].Confirmed
     current_deaths = stats[key].Deaths
+    current_timestamp = stats[key].Date
     //Use reported recovered cases if known...
     current_recovered = stats[key].Recovered
     //...Or provide an estimate as to how many have recovered
@@ -98,7 +132,23 @@ function createTable(table, data) {
         let data_header = document.createTextNode(key)
         th.appendChild(data_header)
         head_row.appendChild(th)
-        //Format
+        //Format stats to HTML page
+        if (key == 'Confirmed Cases') {
+            cell.style.color = canadian_stats.confirmed[0]
+            cell.style.fontSize = canadian_stats.confirmed[1]
+        }
+        if (key == 'Confirmed Deaths') {
+            cell.style.color = canadian_stats.deaths[0]
+            cell.style.fontSize = canadian_stats.deaths[1]
+        }
+        if (key == 'Confirmed Active') {
+            cell.style.color = canadian_stats.active[0]
+            cell.style.fontSize = canadian_stats.active[1]
+        }
+        if (key == 'Total Recovered') {
+            cell.style.color = canadian_stats.recovered[0]
+            cell.style.fontSize = canadian_stats.recovered[1]
+        }
         cell.style.fontWeight = "bold";
         cell.style.textAlign = "center";
         cell.style.padding = "5px";
@@ -113,14 +163,22 @@ container.setAttribute('class', 'container')
 app.appendChild(container)
 getProvinceStats()
 
-//Initialize variables
+// Initialize variables
 let valid_land = false
-total_active = 0
-total_confirmed = 0
-total_deaths = 0
-total_recovered = 0
-current_active = 0
-current_confirmed = 0
-current_deaths = 0
-current_recovered =0
+let total_active = 0
+let total_confirmed = 0
+let total_deaths = 0
+let total_recovered = 0
+let total_timestamp = ''
+
+let current_active = 0
+let current_confirmed = 0
+let current_deaths = 0
+let current_recovered = 0
+let current_timestamp = ''
+
+let accumulated_active = []
+let accumulated_confirmed = []
+let accumulated_deaths = []
+let accumulated_recovered = []
 let selected_display_stats = []
